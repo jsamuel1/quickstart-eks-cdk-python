739e10f4a2081c9bbd803a22ef40389de70ab07f
Joshua Samuel <sauhsoj@amazon.com>
2021-07-27 10:16:42 +1000

    missed adding files

---
+++ cluster-bootstrap/awslbcontroller.py
@@ -0,0 +1,272 @@
+"""
+Purpose
+
+Example of how to provision an EKS cluster, create the IAM Roles for Service Accounts (IRSA) mappings,
+and then deploy various common cluster add-ons (AWS LB Controller, ExternalDNS, EBS/EFS CSI Drivers,
+Cluster Autoscaler, AWS Elasticsearch, Prometheus & Grafana, Calico NetworkPolicy enforceement, 
+OPA Gatekeeper w/example policies, etc.)
+
+NOTE: This pulls many parameters/options for what you'd like from the cdk.json context section.
+Have a look there for many options you can chance to customise this template for your environments/needs.
+"""
+
+from constructs import Construct
+
+from aws_cdk import (
+    Stack, 
+)
+
+class AwsLoadBalancerControllerStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, *, eks_cluster, vpc_id, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        if (self.node.try_get_context("deploy_aws_lb_controller") == "True"):
+            alb_service_account = eks_cluster.add_service_account(
+                "aws-load-balancer-controller",
+                name="aws-load-balancer-controller",
+                namespace="kube-system"
+            )
+
+            # Create the PolicyStatements to attach to the role
+            # Got the required policy from https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.2.0/docs/install/iam_policy.json
+            alb_policy_statement_json_1 = {
+                "Effect": "Allow",
+                "Action": [
+                    "iam:CreateServiceLinkedRole",
+                    "ec2:DescribeAccountAttributes",
+                    "ec2:DescribeAddresses",
+                    "ec2:DescribeAvailabilityZones",
+                    "ec2:DescribeInternetGateways",
+                    "ec2:DescribeVpcs",
+                    "ec2:DescribeSubnets",
+                    "ec2:DescribeSecurityGroups",
+                    "ec2:DescribeInstances",
+                    "ec2:DescribeNetworkInterfaces",
+                    "ec2:DescribeTags",
+                    "ec2:GetCoipPoolUsage",
+                    "ec2:DescribeCoipPools",
+                    "elasticloadbalancing:DescribeLoadBalancers",
+                    "elasticloadbalancing:DescribeLoadBalancerAttributes",
+                    "elasticloadbalancing:DescribeListeners",
+                    "elasticloadbalancing:DescribeListenerCertificates",
+                    "elasticloadbalancing:DescribeSSLPolicies",
+                    "elasticloadbalancing:DescribeRules",
+                    "elasticloadbalancing:DescribeTargetGroups",
+                    "elasticloadbalancing:DescribeTargetGroupAttributes",
+                    "elasticloadbalancing:DescribeTargetHealth",
+                    "elasticloadbalancing:DescribeTags"
+                ],
+                "Resource": "*"
+            }
+            alb_policy_statement_json_2 = {
+                "Effect": "Allow",
+                "Action": [
+                    "cognito-idp:DescribeUserPoolClient",
+                    "acm:ListCertificates",
+                    "acm:DescribeCertificate",
+                    "iam:ListServerCertificates",
+                    "iam:GetServerCertificate",
+                    "waf-regional:GetWebACL",
+                    "waf-regional:GetWebACLForResource",
+                    "waf-regional:AssociateWebACL",
+                    "waf-regional:DisassociateWebACL",
+                    "wafv2:GetWebACL",
+                    "wafv2:GetWebACLForResource",
+                    "wafv2:AssociateWebACL",
+                    "wafv2:DisassociateWebACL",
+                    "shield:GetSubscriptionState",
+                    "shield:DescribeProtection",
+                    "shield:CreateProtection",
+                    "shield:DeleteProtection"
+                ],
+                "Resource": "*"
+            }
+            alb_policy_statement_json_3 = {
+                "Effect": "Allow",
+                "Action": [
+                    "ec2:AuthorizeSecurityGroupIngress",
+                    "ec2:RevokeSecurityGroupIngress"
+                ],
+                "Resource": "*"
+            }
+            alb_policy_statement_json_4 = {
+                "Effect": "Allow",
+                "Action": [
+                    "ec2:CreateSecurityGroup"
+                ],
+                "Resource": "*"
+            }
+            alb_policy_statement_json_5 = {
+                "Effect": "Allow",
+                "Action": [
+                    "ec2:CreateTags"
+                ],
+                "Resource": "arn:aws:ec2:*:*:security-group/*",
+                "Condition": {
+                    "StringEquals": {
+                        "ec2:CreateAction": "CreateSecurityGroup"
+                    },
+                    "Null": {
+                        "aws:RequestTag/elbv2.k8s.aws/cluster": "false"
+                    }
+                }
+            }
+            alb_policy_statement_json_6 = {
+                "Effect": "Allow",
+                "Action": [
+                    "ec2:CreateTags",
+                    "ec2:DeleteTags"
+                ],
+                "Resource": "arn:aws:ec2:*:*:security-group/*",
+                "Condition": {
+                    "Null": {
+                        "aws:RequestTag/elbv2.k8s.aws/cluster": "true",
+                        "aws:ResourceTag/elbv2.k8s.aws/cluster": "false"
+                    }
+                }
+            }
+            alb_policy_statement_json_7 = {
+                "Effect": "Allow",
+                "Action": [
+                    "ec2:AuthorizeSecurityGroupIngress",
+                    "ec2:RevokeSecurityGroupIngress",
+                    "ec2:DeleteSecurityGroup"
+                ],
+                "Resource": "*",
+                "Condition": {
+                    "Null": {
+                        "aws:ResourceTag/elbv2.k8s.aws/cluster": "false"
+                    }
+                }
+            }
+            alb_policy_statement_json_8 = {
+                "Effect": "Allow",
+                "Action": [
+                    "elasticloadbalancing:CreateLoadBalancer",
+                    "elasticloadbalancing:CreateTargetGroup"
+                ],
+                "Resource": "*",
+                "Condition": {
+                    "Null": {
+                        "aws:RequestTag/elbv2.k8s.aws/cluster": "false"
+                    }
+                }
+            }
+            alb_policy_statement_json_9 = {
+                "Effect": "Allow",
+                "Action": [
+                    "elasticloadbalancing:CreateListener",
+                    "elasticloadbalancing:DeleteListener",
+                    "elasticloadbalancing:CreateRule",
+                    "elasticloadbalancing:DeleteRule"
+                ],
+                "Resource": "*"
+            }
+            alb_policy_statement_json_10 = {
+                "Effect": "Allow",
+                "Action": [
+                    "elasticloadbalancing:AddTags",
+                    "elasticloadbalancing:RemoveTags"
+                ],
+                "Resource": [
+                    "arn:aws:elasticloadbalancing:*:*:targetgroup/*/*",
+                    "arn:aws:elasticloadbalancing:*:*:loadbalancer/net/*/*",
+                    "arn:aws:elasticloadbalancing:*:*:loadbalancer/app/*/*"
+                ],
+                "Condition": {
+                    "Null": {
+                        "aws:RequestTag/elbv2.k8s.aws/cluster": "true",
+                        "aws:ResourceTag/elbv2.k8s.aws/cluster": "false"
+                    }
+                }
+            }
+            alb_policy_statement_json_11 = {
+                "Effect": "Allow",
+                "Action": [
+                    "elasticloadbalancing:AddTags",
+                    "elasticloadbalancing:RemoveTags"
+                ],
+                "Resource": [
+                    "arn:aws:elasticloadbalancing:*:*:listener/net/*/*/*",
+                    "arn:aws:elasticloadbalancing:*:*:listener/app/*/*/*",
+                    "arn:aws:elasticloadbalancing:*:*:listener-rule/net/*/*/*",
+                    "arn:aws:elasticloadbalancing:*:*:listener-rule/app/*/*/*"
+                ]                
+            }
+            alb_policy_statement_json_12 = {
+                "Effect": "Allow",
+                "Action": [
+                    "elasticloadbalancing:ModifyLoadBalancerAttributes",
+                    "elasticloadbalancing:SetIpAddressType",
+                    "elasticloadbalancing:SetSecurityGroups",
+                    "elasticloadbalancing:SetSubnets",
+                    "elasticloadbalancing:DeleteLoadBalancer",
+                    "elasticloadbalancing:ModifyTargetGroup",
+                    "elasticloadbalancing:ModifyTargetGroupAttributes",
+                    "elasticloadbalancing:DeleteTargetGroup"
+                ],
+                "Resource": "*",
+                "Condition": {
+                    "Null": {
+                        "aws:ResourceTag/elbv2.k8s.aws/cluster": "false"
+                    }
+                }
+            }
+            alb_policy_statement_json_13 = {
+                "Effect": "Allow",
+                "Action": [
+                    "elasticloadbalancing:RegisterTargets",
+                    "elasticloadbalancing:DeregisterTargets"
+                ],
+                "Resource": "arn:aws:elasticloadbalancing:*:*:targetgroup/*/*"                
+            }
+            alb_policy_statement_json_14 = {
+                "Effect": "Allow",
+                "Action": [
+                    "elasticloadbalancing:SetWebAcl",
+                    "elasticloadbalancing:ModifyListener",
+                    "elasticloadbalancing:AddListenerCertificates",
+                    "elasticloadbalancing:RemoveListenerCertificates",
+                    "elasticloadbalancing:ModifyRule"
+                ],
+                "Resource": "*"
+            }
+            
+            # Attach the necessary permissions
+            alb_service_account.add_to_principal_policy(iam.PolicyStatement.from_json(alb_policy_statement_json_1))
+            alb_service_account.add_to_principal_policy(iam.PolicyStatement.from_json(alb_policy_statement_json_2))
+            alb_service_account.add_to_principal_policy(iam.PolicyStatement.from_json(alb_policy_statement_json_3))
+            alb_service_account.add_to_principal_policy(iam.PolicyStatement.from_json(alb_policy_statement_json_4))
+            alb_service_account.add_to_principal_policy(iam.PolicyStatement.from_json(alb_policy_statement_json_5))
+            alb_service_account.add_to_principal_policy(iam.PolicyStatement.from_json(alb_policy_statement_json_6))
+            alb_service_account.add_to_principal_policy(iam.PolicyStatement.from_json(alb_policy_statement_json_7))
+            alb_service_account.add_to_principal_policy(iam.PolicyStatement.from_json(alb_policy_statement_json_8))
+            alb_service_account.add_to_principal_policy(iam.PolicyStatement.from_json(alb_policy_statement_json_9))
+            alb_service_account.add_to_principal_policy(iam.PolicyStatement.from_json(alb_policy_statement_json_10))
+            alb_service_account.add_to_principal_policy(iam.PolicyStatement.from_json(alb_policy_statement_json_11))
+            alb_service_account.add_to_principal_policy(iam.PolicyStatement.from_json(alb_policy_statement_json_12))
+            alb_service_account.add_to_principal_policy(iam.PolicyStatement.from_json(alb_policy_statement_json_13))
+            alb_service_account.add_to_principal_policy(iam.PolicyStatement.from_json(alb_policy_statement_json_14))
+
+            # Deploy the AWS Load Balancer Controller from the AWS Helm Chart
+            # For more info check out https://github.com/aws/eks-charts/tree/master/stable/aws-load-balancer-controller
+            awslbcontroller_chart = eks_cluster.add_helm_chart(
+                "aws-load-balancer-controller",
+                chart="aws-load-balancer-controller",
+                version="1.2.0",
+                release="awslbcontroller",
+                repository="https://aws.github.io/eks-charts",
+                namespace="kube-system",
+                values={
+                    "clusterName": eks_cluster.cluster_name,
+                    "region": self.region,
+                    "vpcId": vpc_id,
+                    "serviceAccount": {
+                        "create": False,
+                        "name": "aws-load-balancer-controller"
+                    },
+                    "replicaCount": 2
+                }
+            )
+            awslbcontroller_chart.node.add_dependency(alb_service_account)
